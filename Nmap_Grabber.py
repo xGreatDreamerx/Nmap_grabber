b59ef2e743b1bc64f2be0cde4be078d1f61c425f
Matt Hernandez <matthernandez1887@gmail.com>
2018-10-23 07:26:33 -0700

    Cleaner code

--- Nmap_Grabber.py
+++ Nmap_Grabber.py
@@ -1,16 +1,18 @@
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
+#
+#
+#
+#
 report=input('Name of the file of Nmap Scan:\n')
-target_ip=input('Which target is the report needed on\n')
+target_ip=input('Which target is the report needed on?:\n')
 
 
-with open(report,'r') as f:
-    for line in f.readlines():
-        # python can do regexes, but this is for s fixed string only
-        if target_ip in line:
-            idx1 = line.find('"')
-            idx2 = line.find('"', idx1+1)
-            field = line[idx1+1:idx2-1]
-            pprint.pprint(field)
+target_ip = "10.10.100.1"
+begins = "Nmap scan report for"
+
+fhand = open(report,'r')
+for line in fhand:
+    line = line.rstrip()
+    if line.startswith(begins) and line.endswith(target_ip):
+        print(line)
         
            
