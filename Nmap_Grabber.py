f0f73decdb6d44cd313d36a3b3d6dcde3b1d0801
Matt Hernandez <matthernandez1887@gmail.com>
2018-10-25 01:48:53 -0700

    Another long night of coding./n Getting closer to my target.

--- Nmap_Grabber.py
+++ Nmap_Grabber.py
@@ -1,20 +1,31 @@
 #Python3.7.x
 #
 #
-#
-report=input('Name of the file of Nmap Scan:\n')
-target_ip=input('Which target is the report needed on?:\n')
+import linecache
+#report=input('Name of the file of Nmap Scan:\n')
+#target_ip=input('Which target is the report needed on?:\n')
 
-
-target_ip = "10.10.100.1"
+report = "ScanTest.txt"
+target_ip = "10.10.100.2"
 begins = "Nmap scan report for"
-
 fhand = open(report,'r')
-for num1,line in enumerate(fhand, 1):
-    line = line.rstrip()
-    if line.startswith(begins) and line.endswith(target_ip):
-        
-        print(line)
-        print(num1)
+beginsend = "Network Distance:"
 
-           
+for num1,line1 in enumerate(fhand, 1):
+    line1 = line1.rstrip()
+    if line1.startswith(begins) and line1.endswith(target_ip):
+        print(num1)
+        print(line1)
+        break
+for num2,line2 in enumerate(fhand, 1):
+    line2 = line2.rstrip()
+    if line2.startswith(beginsend) and num2 > num1:
+        print(num2)
+        print(line2)
+        break
+with open('ScanTest.txt') as f:
+    linecount = sum(1 for line in f)
+    
+for i in range(num1,num2):
+    
+    print(linecache.getline("ScanTest.txt", i))
--- Testupdatecode.py
+++ Testupdatecode.py
@@ -1,28 +1,20 @@
-#Python3.7.x
-#
-#
-#
-#report=input('Name of the file of Nmap Scan:\n')
-#target_ip=input('Which target is the report needed on?:\n')
+import linecache
+
 report = "ScanTest.txt"
 target_ip = "10.10.100.2"
 begins = "Nmap scan report for"
-fhand = open(report)
+fhand = open(report,'r')
 beginsend = "Network Distance:"
- 
-with fhand as file_txt:
+
+
+
+
+
+for num1,line1 in open(fhand, 1):
+    linecount = sum(1 for line in fhand)
+    if line1.startswith(begins) and line1.endswith(target_ip):
+        print(num1)
     
-    for num1,line1 in enumerate(fhand, 1):
-        line1 = line1.rstrip()
-        if line1.startswith(begins) and line1.endswith(target_ip):
-        
-            print(num1,line1)
-            break
-    for num2,line2 in enumerate(fhand, 1):
-        if line2.startswith('\n') and num2 > num1 :
-        
-            print(num2,line2)
-            #print(line)
-            #print(num1,line1,'\n',num2,line2)    
-            break
-print(num1 , num2)
+for i in range(2,24):
+    
+    print(linecache.getline("ScanTest.txt", i))
